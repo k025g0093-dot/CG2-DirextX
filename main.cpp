4561d31eeb4ea8611c552470a9471bb5b0e125d7
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-04-13 15:04:31 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,7 @@
+﻿#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBoxA(NULL, "Hello, World!", "My First Windows App", MB_OK);
+	return 0;
+}
