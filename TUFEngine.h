cf2da17ae8fab4c6dc8048bbb735d0885e16ad2c
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-05-06 11:37:36 +0900

    エンジンの基礎部分の慣性dmpなどは後々移動予定、とりあえずきれいにできる形に

--- TUFEngine.h
+++ TUFEngine.h
@@ -1,11 +1,50 @@
-// TUFEngine.h
 #pragma once
+#include <Windows.h>
+#include <cstdint>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <filesystem>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"dxguid.lib")
+
 #include "ConvertString.h"
 #include "LogSistem.h"
 #include "DXC.h"
 #include "PSO.h"
 #include "VertexResource.h"
-#include <filesystem>
 
-void InitializeEngine(ID3D12Device* device, HRESULT& hr);
-void FinalizeEngine();
+class TUFEngine {
+public:
+    TUFEngine(int32_t width, int32_t height, HWND hwnd);
+    ~TUFEngine();
+
+    void PreDraw();
+    void PostDraw();
+
+    ID3D12Device* GetDevice() { return device; }
+    ID3D12GraphicsCommandList* GetCommandList() { return commandList; }
+    ID3D12RootSignature* GetRootSignature() { return rootSignature; }
+    ID3D12PipelineState* GetPipelineState() { return pipelineState; }
+
+private:
+    int32_t width = 0;
+    int32_t height = 0;
+
+    IDXGIFactory7* dxgiFactory = nullptr;
+    ID3D12Device* device = nullptr;
+    ID3D12CommandQueue* commandQueue = nullptr;
+    ID3D12CommandAllocator* commandAllocator = nullptr;
+    ID3D12GraphicsCommandList* commandList = nullptr;
+    IDXGISwapChain4* swapChain = nullptr;
+    ID3D12DescriptorHeap* rtvDescriptorHeap = nullptr;
+    ID3D12Resource* swapChainResources[2] = { nullptr };
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2]{};
+    ID3D12RootSignature* rootSignature = nullptr;
+    ID3D12PipelineState* pipelineState = nullptr;
+    HRESULT hr = S_OK;
+
+    void InitializeDXGI(HWND hwnd);
+};
