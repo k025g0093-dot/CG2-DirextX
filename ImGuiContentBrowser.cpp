0b81ab93a0dd9a7dbdc2b89831fb056698c3491c
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-05-28 11:28:11 +0900

    コミット

--- ImGuiContentBrowser.cpp
+++ ImGuiContentBrowser.cpp
@@ -16,6 +16,7 @@ void ImGuiContentBrowser::update(TUFEngine* engine) {
 #ifdef USE_IMGUI
 
 	if (m_DirectoryIcon == -1) {
+		//フォルダの画像とか読み込み
 		m_DirectoryIcon = TextureManager::GetInstance()->LoadTexture("ContentBrowserAsset/Icons/ContentBrowser/DirectoryIcon.png");
 		m_FileIcon = TextureManager::GetInstance()->LoadTexture("ContentBrowserAsset/Icons/ContentBrowser/FileIcon.png");
 	}
