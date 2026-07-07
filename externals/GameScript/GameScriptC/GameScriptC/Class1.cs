60be7bfd5a58fd4cdcb25a3efce66fd355259426
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-07-07 23:57:36 +0900

    exeからC#スクリプトをいじれるように

--- externals/GameScript/GameScriptC/GameScriptC/Class1.cs
+++ externals/GameScript/GameScriptC/GameScriptC/Class1.cs
@@ -5,11 +5,19 @@ namespace GameScriptC
 {
     public class MyScript
     {
+
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine("行けてますね");
+        }
+
         // C++から「Start」ボタンが押されたら呼び出される関数
         [UnmanagedCallersOnly(EntryPoint = "Start")]
         public static void Start()
         {
-            Console.WriteLine("【C#】Start関数がC++から呼び出されました！成功といったがあれは嘘だ！");
+            Console.ReadLine();
+            Console.WriteLine("【C#】Start関数がC++から呼び出されました！ああいったがそれは嘘だ");
         }
 
         // C++から「Update」ボタンが押されたら呼び出される関数
@@ -18,5 +26,7 @@ namespace GameScriptC
         {
            // Console.WriteLine("【C#】再度確認これでいけすか確認");
         }
+
+
     }
 }
